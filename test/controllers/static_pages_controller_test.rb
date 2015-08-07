f80d9cc14921bb618904927f4c3f9dbcf88c21eb
Naohiro IWAMOTO <m232014n@mail.kyutech.jp>
2015-08-07 17:16:20 +0900

    use setup function

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Rails tutorial"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Rails tutorial"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Rails tutorial"
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
