ac271b2b4287a0c73b91a445431820212c8f6d44
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-09-03 18:10:39 +0900

    user login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -32,5 +32,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # アカウント作成に成功したら、users/show が描画されるかテストする
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
